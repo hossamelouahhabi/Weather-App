87a4337395b384b4d34574656133f29355db2188
hossam el ouahhabi <hossamelouahhabi@gmail.com>
2023-12-08 14:38:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  Button,
+  Card,
+  CardContent,
+  Container,
+  Grid,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from "@mui/material";
+import CloudIcon from "@mui/icons-material/Cloud";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Popping"],
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="sm">
+        <Card
+          sx={{
+            minWidth: 275,
+            marginTop: "25vh",
+            background: "#1563ae",
+            color: "white",
+            borderRadius: "15px",
+            padding: 2
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <CardContent>
+            <div style={{ display: "flex", alignItems: "end" }}>
+              <Typography sx={{ ml: 2 }} variant="h2" component="div">
+                Rabat
+              </Typography>
+              <Typography sx={{ ml: 3 }}>12/08/2023</Typography>
+            </div>
+            <hr />
+            <Grid container spacing={2}>
+              <Grid item xs={5}>
+                <Typography variant="h1">
+                  86
+                </Typography>
+              </Grid>
+              <Grid item xs={7}>
+                <CloudIcon sx={{fontSize: 200}}/>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Container>
+    </ThemeProvider>
   );
 }
 
